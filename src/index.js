49c560a91d68bef9a22f8e0690f6a8044c676ac4
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-07-04 18:54:55 +0530

    almost done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RichTextEditor from './RichTextEditor/RichTextEditor';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RichTextEditor/>
   </React.StrictMode>,
   document.getElementById('root')
 );
